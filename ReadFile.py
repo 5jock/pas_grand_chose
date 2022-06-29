403b673a20b3feaf8731170e4b6851ee3250dca2
5jock <107543893+5jock@users.noreply.github.com>
2022-06-29 11:19:21 +0200

    Création fichiers SaveUser.py, groovy_script.groovy, batch_script.bat et ReadFile.py

--- ReadFile.py
+++ ReadFile.py
@@ -1,5 +1,5 @@
 import sys
 
-with open('repertoire_sauv/user.txt') as fichier:
+with open(sys.argv[2]+'\\repertoire_sauv\\user.txt') as fichier:
     ligne = fichier.readline()
 sys.argv[1] = ligne
