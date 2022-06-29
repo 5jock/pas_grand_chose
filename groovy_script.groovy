478f739f3714e2aa04c74efbb72100e2d5b6ffac
5jock <107543893+5jock@users.noreply.github.com>
2022-06-29 11:54:03 +0200

    Création fichiers SaveUser.py, groovy_script.groovy, batch_script.bat et ReadFile.py

--- groovy_script.groovy
+++ groovy_script.groovy
@@ -1,24 +1,35 @@
 node {
+    //    stage('Récupérer repo Github') {
+    //    // Get some code from a GitHub repository
+    //    git 'https://github.com/5jock/pas_grand_chose.git'
+    //}
+    
     wrap([$class: 'BuildUser']) {
-        def user = env.BUILD_USER_ID
+        user = env.BUILD_USER_ID
     }
-}
-node {
-    def mvnHome
-    stage('Récupérer repo Github') {
-        // Get some code from a GitHub repository
-        git 'https://github.com/5jock/pas_grand_chose.git'
-    }
+    
+    echo "$user"
+    chemin = "${env.WORKSPACE}"
+    echo "$chemin"
+    returnedValue = "nanana"
+    echo "$returnedValue"
+    
     stage('Lancer le script Python') {
         run = bat(
             returnStdout: true,
-            script: 'python D:\\Users\\benoit.audinet\\Documents\\Projets\\Python\\Apprentissage\\pas_grand_chose\\SaveUser.py user'
+            script: 'D:\\Users\\benoit.audinet\\Documents\\Projets\\Python\\Apprentissage\\pas_grand_chose\\SaveUser.py '+"${user}"
         )
     }
+    
     stage('Lancer le script .bat') {
-        
+        bat "D:/Users/benoit.audinet/Documents/Projets/Python/Apprentissage/pas_grand_chose/batch_script.bat ${chemin}"
     }
+    
     stage('Afficher dans la console') {
-        
+        run = bat(
+            script: "D:/Users/benoit.audinet/Documents/Projets/Python/Apprentissage/pas_grand_chose/ReadFile.py ${chemin} ${returnedValue}",
+            returnStdout: true
+        )
+        echo "$run"
     }
 }
