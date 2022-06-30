b6c6b1d0f27bfbe0ce9ab449f134cc84b7b2264c
5jock <107543893+5jock@users.noreply.github.com>
2022-06-30 12:10:06 +0200

    Modification du nombre d'arguments de la fonction renvoi_user (fichier ReadFile.py)

--- ReadFile.py
+++ ReadFile.py
@@ -1,11 +1,11 @@
 import sys
 
 
-def renvoi_user(a):
-    with open(a+'\\repertoire_sauv\\user.txt') as fichier:
+def renvoi_user(a, b):
+    with open(a+'\\repertoire_sauv\\user_'+b+'.txt') as fichier:
         ligne = fichier.readline()
     return ligne
 
 
-sys.argv[2] = renvoi_user(sys.argv[1])
+sys.argv[2] = renvoi_user(sys.argv[1], sys.argv[3])
 print(sys.argv[2])
