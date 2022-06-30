3512b1632a84c82bfbf9ade4061249b4cf17de62
5jock <107543893+5jock@users.noreply.github.com>
2022-06-30 12:05:08 +0200

    Création fichiers SaveUser.py, groovy_script.groovy, batch_script.bat et ReadFile.py

--- batch_script.bat
+++ batch_script.bat
@@ -1,3 +1,4 @@
 set chemin=%1
+set nomFichier=%2
 mkdir %chemin%\repertoire_sauv 
-copy %chemin%\user.txt %chemin%\repertoire_sauv
+copy %chemin%\user.txt %chemin%\repertoire_sauv\user_%nomFichier%.txt
